a62ef3ad316033aabb0accaead21d2c673a6b7f2
Xin Lin <131968783+linxin0@users.noreply.github.com>
2025-12-08 14:53:29 -0800

    Update generation.js

--- js/generation.js
+++ js/generation.js
@@ -16,10 +16,5 @@ function llff_carousel_item_template(item) {
                 <div style="width: 100%; text-align: center;">
                     <img src="assets/${item.image}" alt="${item.title}" style="width: 100%; height: auto; border-radius: 8px;">
                 </div>
-                <div class="caption" style="text-align: center; margin-top: 10px;">
-                    <div style="font-weight: 600; font-size: 16px;">
-                        ${item.title}
-                    </div>
-                </div>
             </div>`;
 }
