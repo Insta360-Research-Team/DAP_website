570e5095966526ea1fe4ed6f85c8095f507fb6bf
Xin Lin <131968783+linxin0@users.noreply.github.com>
2025-12-08 14:59:18 -0800

    Update generation.js

--- js/generation.js
+++ js/generation.js
@@ -1,21 +1,22 @@
+// application.js 里
 var llff_items = [
-    { image: "zhuan00.png"},
-    { image: "zhuan01.png"},
-    { image: "zhuan02.png"},
-    { image: "zhuan03.png"},
-    { image: "zhuan04.png"},
-    { image: "zhuan05.png"},
-    { image: "zhuan06.png"},
-    { image: "zhuan07.png"},
-
+    { image: "zhuan00.png" },
+    { image: "zhuan01.png" },
+    { image: "zhuan02.png" },
+    { image: "zhuan03.png" },
+    { image: "zhuan04.png" },
+    { image: "zhuan05.png" },
+    { image: "zhuan06.png" },
+    { image: "zhuan07.png" },
 ];
 
-
-function llff_carousel_item_template(item) {
+// 注意必须写两个参数 (item, info)，虽然 info 不用
+function llff_carousel_item_template(item, info) {
     return `
         <div class="x-card" style="min-width: 400px; margin: 10px;">
             <div style="width: 100%; text-align: center;">
                 <img src="assets/${item.image}" alt="" style="width: 100%; height: auto; border-radius: 8px;">
             </div>
-        </div>`;
+        </div>
+    `;
 }
