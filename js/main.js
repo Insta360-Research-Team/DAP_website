db971992e2fd849b092ddb726cf8d5a385ac4f9c
Songmx <106227069+Willacold@users.noreply.github.com>
2025-12-19 00:36:19 +0800

    Add multiple carousels to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 window.onload = function() {
-    make_carousel('results-llff-carousel', llff_carousel_item_template, llff_items, 1, 1);
+    make_carousel('outdoor-carousel', carousel_item_template, outdoor_items, 1, 1);
+    make_carousel('indoor-carousel', carousel_item_template, indoor_items, 1, 1);
+    make_carousel('details-carousel', carousel_item_template, details_items, 1, 1);
+    make_carousel('robustness-carousel', carousel_item_template, robustness_items, 1, 1);
+    make_carousel('human-carousel', carousel_item_template, human_items, 1, 1);
+    make_carousel('scale-carousel', carousel_item_template, scale_items, 1, 1);
 };
